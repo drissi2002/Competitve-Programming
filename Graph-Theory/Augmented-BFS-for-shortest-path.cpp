665b518df2c7d936951ad162b2f62b72e9c05d67
drissi houcem eddine <84160502+drissi2002@users.noreply.github.com>
2022-02-09 16:26:22 +0100

    Update Augmented-BFS-for-shortest-path.cpp

--- Graph-Theory/Augmented-BFS-for-shortest-path.cpp
+++ Graph-Theory/Augmented-BFS-for-shortest-path.cpp
@@ -1,39 +1,36 @@
-<?xml version="1.0" encoding="UTF-8" standalone="yes" ?>
-<CodeBlocks_project_file>
-	<FileVersion major="1" minor="6" />
-	<Project>
-		<Option title="ProblemSolving" />
-		<Option pch_mode="2" />
-		<Option compiler="gcc" />
-		<Build>
-			<Target title="Debug">
-				<Option output="bin/Debug/ProblemSolving" prefix_auto="1" extension_auto="1" />
-				<Option object_output="obj/Debug/" />
-				<Option type="1" />
-				<Option compiler="gcc" />
-				<Compiler>
-					<Add option="-g" />
-				</Compiler>
-			</Target>
-			<Target title="Release">
-				<Option output="bin/Release/ProblemSolving" prefix_auto="1" extension_auto="1" />
-				<Option object_output="obj/Release/" />
-				<Option type="1" />
-				<Option compiler="gcc" />
-				<Compiler>
-					<Add option="-O2" />
-				</Compiler>
-				<Linker>
-					<Add option="-s" />
-				</Linker>
-			</Target>
-		</Build>
-		<Compiler>
-			<Add option="-Wall" />
-			<Add option="-fexceptions" />
-		</Compiler>
-		<Unit filename="ENICarthage/Sem 2/Programmation/ProblemSolving/Recursion.cpp" />
-		<Unit filename="ENICarthage/Sem 2/Programmation/ProblemSolving/main.cpp" />
-		<Extensions />
-	</Project>
-</CodeBlocks_project_file>
+#include <bits/stdc++.h>
+using namespace std;
+
+const int N = 1e5+7;
+vector<int> graph[N];
+bool visited[N];
+int dist[N];
+
+void augmentedBfs (int src){
+    queue<int> q;
+    q.push(src);
+    visited[src]=true;
+    dist[src]=0;
+
+	while (!q.empty()){
+        int parent = q.front();
+        q.pop();
+        for (auto child:graph[parent])
+        {
+             if (!visited[child])
+             {
+                 visited[child]=true;
+                 q.push(child);
+
+                 dist[child]=1+dist[parent];
+
+             }
+        }
+	}
+}
+
+int main()
+{
+
+    return 0;
+}
